ea2591e405afb26206a8c2168110785129f21734
knexator <daniel.hurtadoramirez314@gmail.com>
2026-09-06 13:31:10 +0200

    broken!: filosofando

--- Tests/PasswordTests.cs
+++ Tests/PasswordTests.cs
@@ -12,6 +12,116 @@ public class PasswordTests {
 	[SetUp]
 	public void Setup() { }
 
+	// hay rulesets cerrados o van cambiando?
+	// un solo ruleset se usa en varios sitios, o en uno solo?
+	// para añadir una nueva regla, donde queremos que viva?
+
+	[Test]
+	public void Approach_MultipleMethods()
+	{
+		string password = "aA12345678";
+		Assert.That(PasswordChecker.IsValidForRuleset1(password), Is.False);
+		Assert.That(PasswordChecker.IsValidForRuleset2(password), Is.True);
+	}
+
+	[Test]
+	public void Approach_Parameter()
+	{
+		string password = "aA12345678";
+		Assert.That(PasswordChecker.IsValid(password, "ruleset_1"), Is.False);
+		Assert.That(PasswordChecker.IsValid(password, "ruleset_2"), Is.True);
+	}
+
+	[Test]
+	public void Approach_ParameterInVariable()
+	{
+		string password = "aA12345678";
+		PasswordChecker checker_ruleset_1 = new PasswordChecker("ruleset_1");
+		PasswordChecker checker_ruleset_2 = new PasswordChecker("ruleset_2");
+		Assert.That(checker_ruleset_1.IsValid(password), Is.False);
+		Assert.That(checker_ruleset_2.IsValid(password), Is.True);
+	}
+
+	[Test]
+	public void Approach_FactoryMethod()
+	{
+		string password = "aA12345678";
+		PasswordChecker checker_ruleset_1 = PasswordCheckerFactory.buildRuleset1();
+		PasswordChecker checker_ruleset_2 = PasswordCheckerFactory.buildRuleset2();
+		Assert.That(checker_ruleset_1.IsValid(password), Is.False);
+		Assert.That(checker_ruleset_2.IsValid(password), Is.True);
+	}
+
+	[Test]
+	public void Approach_RulesetConfig()
+	{
+		string password = "aA12345678";
+		PasswordChecker checker_ruleset_1 = new PasswordChecker(
+			min_length = 8,
+			must_have_uppercase = true,
+			must_have_lowercase = true,
+			must_have_number = true,
+			must_have_underscore = true,
+		);
+		PasswordChecker checker_ruleset_2 = new PasswordChecker(
+			min_length = 6,
+			must_have_uppercase = true,
+			must_have_lowercase = true,
+			must_have_number = true,
+			must_have_underscore = false,
+		);
+		Assert.That(checker_ruleset_1.IsValid(password), Is.False);
+		Assert.That(checker_ruleset_2.IsValid(password), Is.True);
+	}
+
+	[Test]
+	public void Approach_Polymorphism()
+	{
+		string password = "aA12345678";
+ 		// heredan de PasswordChecker
+		PasswordChecker checker_ruleset_1 = new PasswordCheckerForRuleset1();
+		PasswordChecker checker_ruleset_2 = new PasswordCheckerForRuleset2();
+		Assert.That(checker_ruleset_1.IsValid(password), Is.False);
+		Assert.That(checker_ruleset_2.IsValid(password), Is.True);
+	}
+
+	[Test]
+	public void Approach_OOP_PasswordObject()
+	{
+		Password password = new Password("aA12345678");
+		Assert.That(password.HasMoreThanEightCharacters(password), Is.True);
+	}
+
+	[Test]
+	public void Approach_Composition()
+	{
+		string password = "aA12345678";
+		// PasswordChecker tiene una lista de Rule
+		PasswordChecker checker_ruleset_1 = new PasswordChecker();
+		// todos heredan de Rule
+		checker_ruleset_1.addRule(new Rule_MinimumLength(9)); 
+		checker_ruleset_1.addRule(new Rule_HasNumber()); 
+		checker_ruleset_1.addRule(new Rule_HasUnderscore()); 
+		PasswordChecker checker_ruleset_2 = new PasswordChecker();
+		checker_ruleset_2.addRule(new Rule_MinimumLength(7)); 
+		checker_ruleset_2.addRule(new Rule_HasNumber()); 
+		Assert.That(checker_ruleset_1.IsValid(password), Is.False);
+		Assert.That(checker_ruleset_2.IsValid(password), Is.True);
+	}
+
+	[Test]
+	public void Approach_Lambda()
+	{
+		string password = "aA12345678";
+		PasswordChecker checker_ruleset_1 = new PasswordChecker();
+		checker_ruleset_1.addRule((password) => password.ToLower() == password); 
+		checker_ruleset_1.addRule((password) => password.Contains('_')); 
+		PasswordChecker checker_ruleset_2 = new PasswordChecker();
+		checker_ruleset_1.addRule((password) => password.Length > 6); 
+		Assert.That(checker_ruleset_1.IsValid(password), Is.False);
+		Assert.That(checker_ruleset_2.IsValid(password), Is.True);
+	}
+
 	[Test]
 	public void PasswordValid()
 	{
