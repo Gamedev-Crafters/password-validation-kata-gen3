12c381837b0f60e7879c04622a38059710dae8b1
ruins <aaa@aasfasdf.asdfsd>
2026-09-21 16:12:54 +0200

    feat: add validator logic to program

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,7 +1,17 @@
 ﻿namespace Project;
 
 class Program {
-	static void Main(string[] args) {
-		Console.WriteLine("Hello, World!");
+	static void Main(string[] args)
+	{
+		Password password = new Password(args[0]);
+		if (Validator1.ValidatePassword(password))
+		{
+			Console.WriteLine("perfecto");
+		}
+		else
+		{
+			Console.WriteLine("mal");
+		}
+
 	}
 }
